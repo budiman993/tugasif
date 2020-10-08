2f32e8583327aadaa071f022074e4ebafc2dcca6
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-10-08 11:37:21 +0700

    Add files via upload

---
+++ tugasif.php
@@ -0,0 +1,21 @@
+<?php
+$nilai = 50;
+$nilai2 = 7;
+
+if($nilai%2 == 0){
+    echo "$nilai adalah angka genap";
+
+}else{
+    echo "$nilai adalah angka ganjil";
+}
+echo "<br>";
+
+if($nilai2%2 === 0){
+    echo "$nilai2 adalah angka genap";
+
+}else{
+    echo "$nilai2 adalah angka ganjil";
+}
+
+
+?>
